fcc89ec0fb3297c5ab38dba64c95482d38ba54a0
Emma Aboukasm <eabouk@umich.edu>
2017-03-29 12:11:33 -0400

    adding twitter file

--- 206W17_project3.py
+++ 206W17_project3.py
@@ -14,8 +14,8 @@ import twitter_info # same deal as always...
 import json
 import sqlite3
 
-## Your name:
-## The names of anyone you worked with on this project:
+## Your name: Emma Aboukasm
+## The names of anyone you worked with on this project: No one
 
 #####
 
